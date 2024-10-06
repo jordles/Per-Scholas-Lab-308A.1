22de859c2ed9035d8737395e31c14a4f6ddbe230
Jacky Yang <jackyyang1670@gmail.com>
2024-10-05 18:24:26 -0700

    Updates files

--- script.js
+++ script.js
@@ -22,3 +22,58 @@ function recursiveCounter() {
 recursiveCounter();
 
 console.groupEnd();
+
+
+/* -------------------------------------------------------------------------- */
+/*                             PART2: TRAMPOLINES                             */
+/* -------------------------------------------------------------------------- */
+
+console.group("Part 2: Trampolines");
+
+// Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
+
+function recursiveFlatten(arr, result = []) {
+  arr.forEach(el => {
+    if(Array.isArray(el)) {
+      recursiveFlatten(el, result);
+    } else {
+      result.push(el);
+    }
+  })
+
+  return result;
+}
+
+//another solution with reduce
+/* function recursiveFlatten(arr) {
+  return arr.reduce((acc, el) => {
+    if (Array.isArray(el)) {
+      acc.push(...recursiveFlatten(el));
+    } else {
+      acc.push(el);
+    }
+    return acc;
+  }, []);
+} */
+
+console.log(recursiveFlatten([1, [2, [3, [4]]]]));
+
+// Once your recursive function is complete, trampoline it.
+
+//TRAMPOLINE IS IMPLEMENTING TAIL RECURSION (THUNK; a function that wraps the next step of the computation)...
+//TAIL RECURSION IS BASICALLY MAKING THE RECURSIVE CALL THE ONLY LAST THING IN YOUR FUNCTION
+//In trampoline, we convert all nested function calls into a sequential number of functional calls. We wrap the recursive function into a loop and it calls that recursive function until it no more recursive calls are left. 
+
+//by nesting a function inside the recursion call, it is delayed and does not immediately call the recursion. 
+const trampoline = (f, ...args) => {
+  let result = f(...args);
+  while (typeof result === "function") {
+    result = result();
+  }
+  return result;
+}
+console.log("Converting it to a trampoline function...")
+trampoline(recursiveFlatten, [1, [2, [3, [4]]]]);
+
+console.groupEnd();
+
